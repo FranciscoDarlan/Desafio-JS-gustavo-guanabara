ccab4862c8682339625c5499d55b7620594ba3e5
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2023-04-07 11:38:06 -0300

    finalizado-site-de-hora

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const txtParagrafo = document.getElementById('txt-paragrafo')
+function atualizando() {
+    const texto = document.getElementById('txt-paragrafo');
+    const imagem = document.getElementById('imagem');
+    const h1 = document.getElementById('txt-h1');
+    const data = new Date();
+    /*const hora = data.getHours();*/
+    const hora = 18
 
- function troca(){
-    const textNovo = 'Troquei o texto'
-    const trocaTxt = txtParagrafo.value
 
-    trocaTxt.innerHTML = textNovo
+    /* só preciso colocar o innerHRML 1 vez que já troca:*/
+    texto.innerHTML = `Agora são ${hora} horas.`;
+
+
+    if (hora >= 00 && hora < 12) {
+        imagem.src = "./bomdia.png";
+        /*troca css*/
+        document.body.style.background = '#fd9200'
+
+
+    } else if (hora >= 12 && hora < 18) {
+        imagem.src = "./boatarde.jpg";
+        document.body,style.background = '#91cae8'
+
+    } else {
+        (hora >= 18 && hora < 24)
+       imagem.src = "./boanoite.jpg";
+       document.body.style.background = '#5e544b'
+    }
+
 }
-
-console.log(troca)
